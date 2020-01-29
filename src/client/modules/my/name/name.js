f10f3a8c6c6734880f22d350e8d1ba23a86af8ee
Bret Little <bret.little@gmail.com>
2020-01-29 12:09:36 -0500

    Remove stuff

--- src/client/modules/my/name/name.js
+++ src/client/modules/my/name/name.js
@@ -3,20 +3,6 @@ import { LightningElement, api } from 'lwc';
 export default class Name extends LightningElement {
     @api
     name = 'John';
-    employer = 2;
-
-    companies = [
-        { id: 1, name: 'Salesforce' },
-        { id: 2, name: 'Commerce cloud' },
-        { id: 3, name: 'Shoppify' }
-    ];
-
-    get viewCompanies() {
-        return this.companies.map(company => ({
-            ...company,
-            selected: this.employer === company.id
-        }));
-    }
 
     update(event) {
         this.dispatchEvent(
