56d1c23569d12ec27f1f929411a436e670c50e5f
Bret Little <bret.little@gmail.com>
2020-02-05 09:46:08 -0500

    5

--- src/client/modules/my/profile/profile.js
+++ src/client/modules/my/profile/profile.js
@@ -9,4 +9,20 @@ export default class Employer extends LightningElement {
 
     @api
     name;
+
+    @api
+    employer;
+
+    companies = [
+        { id: '1', name: 'Salesforce' },
+        { id: '2', name: 'Commerce Cloud' },
+        { id: '3', name: 'Community Cloud' }
+    ];
+
+    get viewCompanies() {
+        return this.companies.map(company => ({
+            ...company,
+            selected: this.employer === company.id
+        }));
+    }
 }
